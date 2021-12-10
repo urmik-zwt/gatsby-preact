d5f08e70729bdbb9ce53f2e05f9791d32638be9f
Urmik Prajapati <urmik@zealousweb.com>
2021-12-10 21:44:48 +0530

    gatsby preact issue

---
+++ src/slices/index.js
@@ -0,0 +1,9 @@
+import { ImageCaption } from './ImageCaption'
+import { Quote } from './Quote'
+import { Text } from './Text'
+
+export const components = {
+  image_with_caption: ImageCaption,
+  quote: Quote,
+  text: Text,
+}
